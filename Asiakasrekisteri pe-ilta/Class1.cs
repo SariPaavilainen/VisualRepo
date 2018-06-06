1423c207a1e085f194cda1c5a0f6e41162264fc4
SariPaavilainen <sari.paavilainen@iki.fi>
2018-06-06 14:24:25 +0300

    Add project files.

---
+++ Asiakasrekisteri pe-ilta/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asiakasrekisteri_pe_ilta
+{
+        class Asiakas
+        {
+            public static int SeuraavaVapaaNumero = 1000;
+            public int Asiakasnumero { get; set; }
+
+            public string AsiakkaanNimi { get; set; }
+
+            public decimal TilaustenArvo { get; set; }
+
+
+
+            public Asiakas(string nimi, decimal tilaukset)
+            {
+                Asiakasnumero += SeuraavaVapaaNumero;
+                AsiakkaanNimi = nimi;
+                TilaustenArvo = tilaukset;
+            SeuraavaVapaaNumero++;
+
+            }
+        public static decimal TilaustenYhteissumma(List<Asiakas> asiakaslista)
+        {
+            decimal summalaskuri = 0m;
+            foreach (Asiakas dummy in asiakaslista)
+            {
+                summalaskuri += dummy.TilaustenArvo;
+            }
+
+            return summalaskuri;
+
+        }
+        public override string ToString()
+        {
+            return $"{Asiakasnumero}: {AsiakkaanNimi}, tilausten arvo {TilaustenArvo} e.";
+        }
+
+
+    }
+    
+}
---
+++ Asiakasrekisteri pe-ilta/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asiakasrekisteri_pe_ilta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Asiakas> asiakkaat = new List<Asiakas>();
+            Asiakas a1 = new Asiakas("Ville Virtanen", 3544.20m);
+            Asiakas a2 = new Asiakas("Taimi Tuiskunen", 9554.74m);
+            Asiakas a3 = new Asiakas("Leena Lundahl", 611.40m);
+            Asiakas a4 = new Asiakas("Pyry Peltonen", 45978.12m);
+
+            asiakkaat.Add(a1);
+            asiakkaat.Add(a2);
+            asiakkaat.Add(a3);
+            asiakkaat.Add(a4);
+
+            foreach (Asiakas dummy in asiakkaat)
+            {
+                Console.WriteLine(dummy.ToString());
+            }
+            Console.WriteLine("Tilausten yhteissumma on " + (Asiakas.TilaustenYhteissumma(asiakkaat)));
+
+
+            StreamWriter kirjuri = new StreamWriter("c:\\users\\Sari\\source\\repos\\Academy\\Viikko 1\\asiakasrekisteri.xml");
+            kirjuri.WriteLine("<<Asiakkaat>>");
+            foreach (var item in asiakkaat)
+            {
+                kirjuri.WriteLine("\t<Asiakas>");
+                kirjuri.WriteLine("\t\t<Asiakasnumero>" + item.Asiakasnumero + "<\\Asiakasnumero>");
+                kirjuri.WriteLine("\t\t<AsiakkaanNimi>" + item.AsiakkaanNimi + "<\\AsiakkaanNimi>");
+                kirjuri.WriteLine("\t\t<TilaustenArvo>" + item.TilaustenArvo + "<\\TilaustenArvo>");
+                kirjuri.WriteLine("----");
+            }
+
+            kirjuri.Close();
+            
+
+
+        }
+    }
+}
